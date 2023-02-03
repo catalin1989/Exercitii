87cffbe43b6dbf14a31c8d5e6909f050483cb3af
catalin1989 <catalin_chira05@yahoo.com>
2023-02-03 16:26:42 +0200

    declaring methods

--- Racing_Game/Racing_Game/src/main/java/org/example/Vehicle.java
+++ Racing_Game/Racing_Game/src/main/java/org/example/Vehicle.java
@@ -9,4 +9,21 @@ public class Vehicle {
     double maxSpeed;
     boolean damaged;
     String color;
+
+    public double accelerate(double speed, double durationInHours){
+
+        System.out.println(name+"is accelerating with "+speed+"km/h "+ durationInHours+"h");
+
+        double distance= speed*durationInHours;
+
+        totalTravelDistance=totalTravelDistance+distance;
+
+        System.out.println("Total travel distance for vehicle "+ name+": "+totalTravelDistance);
+        double usedFuel=distance*mileage/100;
+        fuelLevel-=usedFuel;
+        System.out.println("Remaining fuel for vehicle "+ name+": "+fuelLevel);
+
+
+        return distance;
+    }
 }
